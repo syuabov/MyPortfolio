9f8a8cf7c2a61b5d8b5b3692bcfdc71f2696e5a5
syuabov <syuabov03@gmail.com>
2023-03-08 11:52:18 -0500

    done for now

--- src/screens/Portfolio/projects.js
+++ src/screens/Portfolio/projects.js
@@ -1,4 +1,6 @@
 import jewelryWebsite from "./images/jewelryWebsite.jpg";
+import resume from "./images/resume.jpg";
+import resumePdf from "./images/Samuel Yuabov Resume.pdf";
 
 export const projects = [
     {
@@ -6,5 +8,11 @@ export const projects = [
         description: "This is a test ecommerce website im working on for a jewelry store using HTML, CSS, JS, PHP and MySQL Database.",
         image: jewelryWebsite,
         url: "https://syuabovtest.000webhostapp.com/TestApp/index.php",
+    },
+    {
+        title: "Resume",
+        description: "This is my current resume",
+        image: resume,
+        url: resumePdf,
     }
 ]
